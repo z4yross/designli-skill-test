6330de8cadf65cb1d67f7119462fb3152a9128ee
z4yross <z4yross@gmail.com>
2024-01-05 12:52:42 -0500

    parser module creation

--- email-parser/src/app.module.ts
+++ email-parser/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { ConfigModule } from '@nestjs/config';
+import { ParserModule } from './parser/parser.module';
 
 @Module({
-    imports: [ConfigModule.forRoot()],
+    imports: [ConfigModule.forRoot(), ParserModule],
     controllers: [AppController],
     providers: [AppService],
 })
