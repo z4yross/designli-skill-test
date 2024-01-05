945be050fc55ce090429722fcbd4cda281ab7f7b
z4yross <z4yross@gmail.com>
2024-01-05 16:36:25 -0500

    clean config in app

--- email-parser/src/main.ts
+++ email-parser/src/main.ts
@@ -3,9 +3,7 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, {
-        rawBody: true,
-    });
+    const app = await NestFactory.create(AppModule);
 
     const configService = app.get<ConfigService>(ConfigService);
 
